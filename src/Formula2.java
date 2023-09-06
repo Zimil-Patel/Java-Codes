03fb25ee7d25fa110e22bed5051ac620235574f5
Zimil Patel <patelzimil2310@gmail.com>
2023-09-06 19:22:10 +0530

    06-09-2023

---
+++ src/Formula2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Formula2 {
+
+    public static void main(String[] args){
+
+        short x, y;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter value of x :");
+        x = input.nextShort();
+
+        System.out.print("Enter value of y :");
+        y = input.nextShort();
+
+        System.out.println("(x - y)^2 = "+(short)((x * x)-(2 * x * y)+(y * y)));
+
+    }
+}
