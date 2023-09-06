f980bfd96270e18550cbddb80b19122c29b8210b
Zimil Patel <patelzimil2310@gmail.com>
2023-09-06 19:42:40 +0530

    06-09-2023

---
+++ src/Formula6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Formula6 {
+
+    public static void main(String[] args){
+
+        short x, y, z;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter value of x :");
+        x = input.nextShort();
+
+        System.out.print("Enter value of y :");
+        y = input.nextShort();
+
+        System.out.print("Enter value of y :");
+        z = input.nextShort();
+
+        System.out.println("(x + y + z)^3 = "+(short)((x * x * x) + (y * y * y) + (z * z * z) + 3 * ((x + y) * (y + z) * (z + x))));
+    }
+}
