1acc54b62786bbf7ac25944ad9e0440d111304bb
Zimil Patel <patelzimil2310@gmail.com>
2023-09-06 19:28:24 +0530

    06-09-2023

---
+++ src/Formula3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Formula3 {
+
+    public static void main(String[] args){
+
+        short x, y;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter value of x :");
+        x = input.nextShort();
+
+        System.out.print("Enter value of y :");
+        y = input.nextShort();
+
+        System.out.println("(x + y)^3 = "+(short)((x * x * x) + (y * y * y) + (3*(x * x)*y) + (3 * x *(y * y))));
+
+    }
+}
