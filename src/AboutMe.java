080400840d059b8216ee5b8441421fad85747551
Zimil Patel <112332000+Zimil-Patel@users.noreply.github.com>
2023-09-07 19:34:12 +0530

    AboutMe.java

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -2,7 +2,7 @@ public class AboutMe {
 
     public static void main(String[] args){
 
-        System.out.println("\nName : Zimil Patel\nAge : 21\nSchool : LD High School");
+        System.out.println("Name : Zimil Patel\nAge : 21\nSchool : LD High School");
 
     }
 }
