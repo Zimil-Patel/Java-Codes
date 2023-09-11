af9a9cd47838599e0da3ff8e5aea5daa69dd0918
Zimil Patel <patelzimil2310@gmail.com>
2023-09-11 16:37:43 +0530

    11-09-2023

---
+++ src/FindMinMax.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class FindMinMax {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        Byte num1, num2;
+
+        System.out.print("Enter value of num1 :");
+        num1 = input.nextByte();
+
+        System.out.print("Enter value of num2 :");
+        num2= input.nextByte();
+
+        if(num1 != num2){
+            if(num1 > num2){
+                System.out.println("num1("+num1+") is Max number!");
+                System.out.println("num2("+num2+") is Min number!");
+            } else{
+                System.out.println("num2("+num2+") is Max number!");
+                System.out.println("num1("+num1+") is Min number!");
+            }
+        } else {
+            System.out.println("both numbers are equals!");
+        }
+    }
+
+}
