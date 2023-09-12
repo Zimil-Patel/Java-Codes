dc415f64ca9f45212e248ae181033dfb9b290ccd
Zimil Patel <patelzimil2310@gmail.com>
2023-09-12 16:19:14 +0530

    12-09-2023

---
+++ src/LMaxOfFour.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class LMaxOfFour {
+
+    public static void main(String[] args){
+
+        short num1, num2, num3, num4;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the value of num1 : ");
+        num1 = input.nextShort();
+
+        System.out.print("Enter the value of num2 : ");
+        num2 = input.nextShort();
+
+        System.out.print("Enter the value of num3 : ");
+        num3 = input.nextShort();
+
+        System.out.print("Enter the value of num4 : ");
+        num4 = input.nextShort();
+
+        if(num1 > num2 && num1 > num3 && num1 > num4){
+
+            System.out.println("num1("+num1+") is maximum");
+
+        } else if(num2 > num1 && num2 > num3 && num2 > num4){
+
+            System.out.println("num2("+num2+") is maximum");
+
+        } else if(num3 > num1 && num3 > num2 && num3 > num4){
+
+            System.out.println("num3("+num3+") is maximum");
+        } else if(num4 > num1 && num4 > num2 && num4 > num3){
+
+            System.out.println("num4("+num4+") is maximum");
+        }
+    }
+}
