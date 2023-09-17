5f0b019724aba0e49a6265492da691c612406cd5
Zimil Patel <patelzimil2310@gmail.com>
2023-09-17 20:47:51 +0530

    17-09-2023

---
+++ src/TernaryEvenOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TernaryEvenOdd {
+
+    public static void main(String[] args){
+
+        short x;
+        String result;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter value of x : ");
+        x = input.nextShort();
+
+        result = (x % 2 == 0) ? "Even number!" : "Odd number!";
+
+        System.out.println(result);
+    }
+}
