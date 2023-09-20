655b436c965b5396f0d7c5b73203900d422c3d09
Zimil Patel <patelzimil2310@gmail.com>
2023-09-20 22:17:08 +0530

    20-09-2023

---
+++ src/While_1To10.java
@@ -0,0 +1,14 @@
+public class While_1To10 {
+
+    public static void main(String[] args){
+
+        byte x = 1;
+
+        while (x <= 10){
+
+            System.out.println(x);
+            x++;
+
+        }
+    }
+}
