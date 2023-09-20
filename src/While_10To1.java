e33e473b0befbd4ac89f64cedb36d589e40911ca
Zimil Patel <patelzimil2310@gmail.com>
2023-09-20 22:22:21 +0530

    20-09-2023

---
+++ src/While_10To1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class While_10To1 {
+
+    public static void main(String[] args){
+
+        byte n;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the value of n : ");
+        n = in.nextByte();
+
+        while(n > 0){
+
+            System.out.println(n--);
+
+        }
+
+    }
+}
