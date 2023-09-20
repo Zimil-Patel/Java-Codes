e8d4b76d056aeab325565854b1a68c579dd3bebd
Zimil Patel <patelzimil2310@gmail.com>
2023-09-20 22:37:32 +0530

    20-09-2023

--- src/While_Factorial.java
+++ src/While_Factorial.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+
 public class While_Factorial {
 
+    public static void main(String[] args){
+
+        byte x = 1, n;
+        int factorial = 1;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the value of n : ");
+        n = in.nextByte();
+
+        while(x <= n){
+
+            factorial *= x;
+            x++;
+
+        }
+
+        System.out.println("factorial of 1 to "+n+" is "+factorial);
+
+    }
 
 }
