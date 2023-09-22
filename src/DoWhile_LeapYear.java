53552cd4743bb0c203e3b59e6cea50bda06ebf2e
Zimil Patel <patelzimil2310@gmail.com>
2023-09-22 12:26:51 +0530

    22-09-2023

---
+++ src/DoWhile_LeapYear.java
@@ -0,0 +1,16 @@
+public class DoWhile_LeapYear {
+
+    public static void main(String[] args){
+
+        short x = 2000;
+        System.out.println("Leap Years 2000-3000 : ");
+        do{
+
+            if (x % 4 == 0)
+                System.out.println(x);
+            x++;
+        } while(x <= 3000);
+
+    }
+
+}
