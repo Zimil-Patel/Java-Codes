8e7de2cb281539f20b74e917ccd1e9d201449cb4
Zimil Patel <patelzimil2310@gmail.com>
2023-09-22 12:22:06 +0530

    22-09-2023

---
+++ src/DoWhile_Odd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DoWhile_Odd {
+
+    public static void main(String[] args){
+
+        short x = 1, n;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the value of n :");
+        n = in.nextShort();
+
+        do {
+            if( x % 2 != 0)
+                System.out.println(x);
+
+            x++;
+        } while (x <= n);
+
+
+    }
+
+}
