b23757d3cb9b43f78183f730d88769074debd63e
Zimil Patel <patelzimil2310@gmail.com>
2023-09-22 12:24:52 +0530

    22-09-2023

---
+++ src/DoWhile_Even.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DoWhile_Even {
+
+    public static void main(String[] args){
+
+        short n;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the value of n : ");
+        n = in.nextShort();
+
+        do {
+            if (n % 2 == 0)
+                System.out.println(n);
+            n--;
+        } while (n > 0);
+
+    }
+
+}
