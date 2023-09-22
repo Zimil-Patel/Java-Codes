5bb344489f777704a10fb5b11efef34895a2e405
Zimil Patel <patelzimil2310@gmail.com>
2023-09-22 12:13:08 +0530

    22-09-2023

---
+++ src/DoWhile1To10.java
@@ -0,0 +1,14 @@
+public class DoWhile1To10 {
+
+    public static void main(String[] args){
+
+        byte x = 1;
+
+        do{
+            System.out.println(x);
+            x++;
+        } while(x <= 10);
+
+    }
+
+}
