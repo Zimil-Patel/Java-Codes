d56c957e99b2ad3a35eb28237cb0f2724daf2639
Zimil Patel <patelzimil2310@gmail.com>
2023-09-23 15:00:23 +0530

    23-09-2023

---
+++ src/For_Table.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class For_Table {
+
+    public static void main(String[] args){
+
+        short x, n;
+
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.print("Enter the value of n : ");
+        n = in.nextShort();
+
+        for (x = 1 ; x <= 10; x++){
+
+            System.out.println(n+" * "+x+" = "+(n*x));
+
+        }
+
+
+    }
+
+}
