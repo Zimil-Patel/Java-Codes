dc4ba0721d28db2b5dc607e63a6bcea965c0933a
Zimil Patel <patelzimil2310@gmail.com>
2023-09-23 14:27:06 +0530

    23-09-2023

---
+++ src/For_10To1.java
@@ -0,0 +1,14 @@
+public class For_10To1 {
+
+    public static void main(String[] args){
+
+        byte x;
+
+        for(x = 10; x > 0; x--){
+
+            System.out.println(x);
+
+        }
+
+    }
+}
---
+++ src/For_1To10.java
@@ -0,0 +1,13 @@
+public class For_1To10 {
+
+    public static void main(String[] args){
+
+        byte x;
+
+        for(x = 1; x <= 10; x++){
+
+            System.out.println(x);
+        }
+
+    }
+}
