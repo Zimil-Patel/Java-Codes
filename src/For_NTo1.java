18f1d068d9668e12ed0d3ac1324d9299240461df
Zimil Patel <patelzimil2310@gmail.com>
2023-09-23 14:35:37 +0530

    23-09-2023

---
+++ src/For_NTo1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class For_NTo1 {
+
+    public static void main(String[] args){
+
+
+        short n;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the value of n :");
+        n = in.nextShort();
+
+        for(;n > 0; n--){
+
+            System.out.println(n);
+
+        }
+
+    }
+}
